50c08add9ae3a8d175f48ffd2481bca6c166d646
pournima108 <mishra.pournima108@gmail.com>
2018-04-04 19:17:07 +0530

    Revert "first commit"
    This reverts commit 1ec5617e92963ae7a8e96736d07932f74b8a4668.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Request = require("request");
 var bodyParser = require('body-parser');
 var Routing = require('./Routing/Post-result.js');
 var getData = require('./Routing/Get-api.js');
-//var zChat = require('./web_sdk');
+
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
@@ -13,10 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-// Chat.init({
-//     account_key: 'EK7YOWLBbzWm8JOEzn81U8AQcAIHbwLU'
-// });
 
 app.get('/', function (req, res) {
     res.send(__dirname + "/index.html" );
